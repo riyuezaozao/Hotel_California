df3ab0bf5edc96475bfacaae5be1fd87ac442c5f
Bhavitha Dasaram <bdasaram@stevens.edu>
2016-11-01 22:46:54 -0400

    added studentid[] in assignment class

--- assignment.h
+++ assignment.h
@@ -6,9 +6,11 @@ class assignment
 {
 private:
     double grade[40]={0}; //storing all the grade for each student in this specific assignment
+    int student_id[40];
 public:
     assignment();
     void setGrade(double homeworkgrade); //homeworkgrade is a function of class data
+    //homeworkgrade should be an array right? Also shouldn't we store
     double getGrade();
 
     //I think we can put all this statistic calculation into another class file
